9437af47a8011e90051b2f26f27345d6036c718e
Guilherme S. Marinho <66325544+m4r1nho@users.noreply.github.com>
2022-05-14 20:16:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# main
+# Importing the classes
+from stack import Stack
+from queue import Queue
+
+stack01 = Stack([])  # the first stack
+queue01 = Queue([])  # the first queue
+
+# To use the functions
+stack01.create_stack()
+stack01.function_add()
+stack01.function_remove()
+stack01.function_confer()
+stack01.function_clean()
+stack01.function_confer()
+
+# To use the functions
+queue01.create_stack()
+queue01.function_add()
+queue01.function_remove()
+queue01.function_confer()
+queue01.function_clean()
+queue01.function_confer()
---
+++ stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self, vector):  #
+        self.vector = vector
+
+    # ↓ function to create a stack
+    def create_stack(self):
+        size = int(input("What the size? "))  # Entering the size
+        for i in range(size):  # loop
+            n = float(input(f"Enter the value {i + 1}: "))  # the value for position
+            self.vector.append(n)  # adding in the vector
+        return self.vector
+
+    # ↓ function to add in stack, increase the stack
+    def function_add(self):
+        new_value = float(input("Enter the new value: "))  # the new and last value
+        self.vector.append(new_value)  # adding in the vector
+        return self.vector
+
+    # ↓ LIFO, Last In, First Out
+    def function_remove(self):
+        print("Removing value ")
+        self.vector.pop()
+        return self.vector
+
+    # ↓ checking the top
+    def function_confer(self):
+        print(self.vector)
+        if len(self.vector) > 0:
+            top_value = self.vector[len(self.vector) - 1]
+            print(f'In the top:{top_value} ')
+
+    # cleaning it
+    def function_clean(self):
+        print("Cleaning ")
+        self.vector = []
+        return self.vector
